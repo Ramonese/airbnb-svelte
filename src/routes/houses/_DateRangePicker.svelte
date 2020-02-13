47319cd849f0396d889f98158c1b44c628b0990c
Ieva Ozolite <ieva.ozolite@visma.com>
2020-02-13 17:26:37 +0200

    update calendar functionality

--- src/routes/houses/_DateRangePicker.svelte
+++ src/routes/houses/_DateRangePicker.svelte
@@ -1,52 +1,80 @@
 <script>
-import Datepicker from "../../lib/svelte-calendar-1.0.10/src/Components/Datepicker.svelte";
-const dateFormat = '#{l}, #{F} #{j}, #{Y}';
-const startDateSelectableCallback = date => {
-  return true
-}
-let startDate = new Date();
-const endDateSelectableCallback = date => {
-  const today = new Date()
-  if (date.getTime() - startDate.getTime() < 0) {
-    return false
-  }
-  if (date.getFullYear() === today.getFullYear() &&
-    date.getMonth() === today.getMonth() &&
-    date.getDate() === today.getDate()) {
-      return false
-  }
-  if (date.getFullYear() === today.getFullYear() &&
-    date.getMonth() === today.getMonth() &&
-    date.getDate() === today.getDate()) {
-      return false
-  }
-  return true
-}
-let endDate = new Date();
+  import Datepicker from "../../lib/svelte-calendar-1.0.10/src/Components/Datepicker.svelte";
+  const dateFormat = "#{l}, #{F} #{j}, #{Y}";
+  let startDate = new Date();
+  let endDate = new Date(startDate.getTime() + 1000 * 3600 * 24);
+  const startDateSelectableCallback = date => {
+    return true;
+  };
+  const firstDateIsPastDayComparedToSecond = (firstDate, secondDate) => {
+    if (firstDate.setHours(0, 0, 0, 0) - secondDate.setHours(0, 0, 0, 0) >= 0) {
+      //first date is in future, or it is today
+      return false;
+    }
+    return true;
+  };
+  let endDateSelectableCallback = date => {
+    const today = new Date();
+    if (date.getTime() - startDate.getTime() < 0) {
+      return false;
+    }
+    if (
+      date.getFullYear() === today.getFullYear() &&
+      date.getMonth() === today.getMonth() &&
+      date.getDate() === today.getDate()
+    ) {
+      return false;
+    }
+    if (
+      date.getFullYear() === startDate.getFullYear() &&
+      date.getMonth() === startDate.getMonth() &&
+      date.getDate() === startDate.getDate()
+    ) {
+      return false;
+    }
+    return true;
+  };
 </script>
+
 <style>
-.date-range-picker-container {
-  border: 1px solid #ddd;
-  display: grid;
-  grid-template-columns: 40% 20% 40%;
-  padding: 10px;
-}
+  .date-range-picker-container {
+    border: 1px solid #ddd;
+    display: grid;
+    grid-template-columns: 40% 20% 40%;
+    padding: 10px;
+  }
 </style>
+
 <div class="date-range-picker-container">
-  <Datepicker 
-    format={dateFormat} 
-    start={new Date()} 
-    selectableCallback={startDateSelectableCallback}  
-    on:dateSelected={e => { startDate = new Date(e.detail.date) }}>
-    <div class="check-in">{`${startDate.getDate()} ${startDate.toLocaleString('default', { month: 'short' })}`}</div>
+  <Datepicker
+    format={dateFormat}
+    start={new Date()}
+    selectableCallback={startDateSelectableCallback}
+    on:dateSelected={e => {
+      startDate = new Date(e.detail.date);
+      if (!firstDateIsPastDayComparedToSecond(startDate, endDate)) {
+        endDate = new Date(startDate.getTime() + 1000 * 3600 * 24);
+      }
+      endDateSelectableCallback = endDateSelectableCallback;
+    }}>
+    <div class="check-in">
+      {`${startDate.getDate()} ${startDate.toLocaleString('default', {
+        month: 'long'
+      })}`}
+    </div>
   </Datepicker>
   <div class="arrow">➡️</div>
   <Datepicker
-    format={dateFormat} 
-    start={new Date()} 
-     selectableCallback={endDateSelectableCallback}  
-     on:dateSelected={e => { endDate = new Date(e.detail.date) }}
-    >
-    <div class="check-out">{`${endDate.getDate()} ${endDate.toLocaleString('default', { month: 'short' })}`}</div>
+    format={dateFormat}
+    start={new Date()}
+    selectableCallback={endDateSelectableCallback}
+    on:dateSelected={e => {
+      endDate = new Date(e.detail.date);
+    }}>
+    <div class="check-in">
+      {`${endDate.getDate()} ${endDate.toLocaleString('default', {
+        month: 'long'
+      })}`}
+    </div>
   </Datepicker>
 </div>
